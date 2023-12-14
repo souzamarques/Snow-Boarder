579ebac40073161fa851b86a9a9e967bcbb677d9
Lucas Souza <lucassouzzamarques@gmail.com>
2023-12-14 01:41:13 -0300

    Triggering Crash Effect and Finish Effect

--- Snow Boarder/Assets/Scripts/CrashDetector.cs
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -5,12 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] ParticleSystem crashEffect;
     [SerializeField] float loadDelay = 1.3f;
 
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", loadDelay);
         }    
     }
--- Snow Boarder/Assets/Scripts/Finish.cs
+++ Snow Boarder/Assets/Scripts/Finish.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
+    [SerializeField] ParticleSystem finishEffect;
     [SerializeField] float loadDelay = 1.3f;
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        finishEffect.Play();
         Invoke("ReloadScene", loadDelay); 
     }
 
